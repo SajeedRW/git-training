c9903e73679beaae2465993406dc9cd1fa62c789
Jamil Ansari <Jamil.ansari@email.com>
2024-11-03 02:00:22 +0530

    code added py

--- example2.py
+++ example2.py
@@ -1,18 +1,9 @@
-</document-id>
-      <document-id data-format="docdb" id="312182343">
-        <doc-number>A5177</doc-number>
-      </document-id>
-      <document-id data-format="epodoc">
-        <doc-number>ATA5177</doc-number>
-      </document-id>
-    </publication-reference>
-    <application-reference>
-      <document-id>
-        <country>AT</country>
-        <doc-number>5177</doc-number>
-        <kind>A</kind>
-        <date>19770107</date>
-      </document-id>
-      <document-id data-format="docdb" id="548388">
-        <doc-number>5177</doc-number>
-      </document-id>
+# Taking kilometers input from the user
+kilometers = float(input("Enter value in kilometers: "))
+
+# conversion factor
+conv_fac = 0.621371
+
+# calculate miles
+miles = kilometers * conv_fac
+print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
